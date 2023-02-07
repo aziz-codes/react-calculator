ea5b5d4578ad806f68dd0c1b1765a17bfbf10337
aziz-codes <azizisoffline@gmail.com>
2023-02-07 23:56:50 +0500

    use reducer error

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,7 +1,48 @@
-import React from "react";
-
+import React, { useReducer } from "react";
+const reducer = (state, action) => {
+  if (action.type === "+") {
+    console.log(state.num1 + state.num2);
+  }
+};
 const Test = () => {
-  return <div>Test</div>;
+  const [state, dispatch] = useReducer(reducer, { num1: 30, num2: 30 });
+  return (
+    <div className="my-20 mx-40 flex flex-col gap-4">
+      {/* <label>{state.age}</label> */}
+      <button
+        className="bg-sky-500 p-1 rounded-md"
+        onClick={() => {
+          dispatch({ type: "+" });
+        }}
+      >
+        +
+      </button>
+      <button
+        className="bg-sky-500 p-1 rounded-md"
+        onClick={() => {
+          dispatch({ type: "-" });
+        }}
+      >
+        -
+      </button>
+      <button
+        className="bg-sky-500 p-1 rounded-md"
+        onClick={() => {
+          dispatch({ type: "*" });
+        }}
+      >
+        *
+      </button>
+      <button
+        className="bg-sky-500 p-1 rounded-md"
+        onClick={() => {
+          dispatch({ type: "/" });
+        }}
+      >
+        /
+      </button>
+    </div>
+  );
 };
 
 export default Test;
